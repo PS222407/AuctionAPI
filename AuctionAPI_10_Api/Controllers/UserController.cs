909a1758b4d35598b66bd54c3844252b7889c190
jens <jensramakers@gmail.com>
2024-03-08 17:23:36 +0100

    added api/v1 prefix

--- AuctionAPI_10_Api/Controllers/UserController.cs
+++ AuctionAPI_10_Api/Controllers/UserController.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace AuctionAPI_10_Api.Controllers;
 
+[Route("api/v1/[controller]")]
+[ApiController]
 public class UserController : Controller
 {
     private readonly IUserService _userService;
