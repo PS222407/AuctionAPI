96c39ae0c5ae03e785ba6505cba4dc9df728aef5
jens <jensramakers@gmail.com>
2024-03-31 12:55:09 +0200

    removed test to speed up ci to test

--- IntegrationTests/UserEndpointTests.cs
+++ IntegrationTests/UserEndpointTests.cs
@@ -1,33 +1,34 @@
-﻿using System.Net;
-using System.Net.Http.Headers;
-using System.Net.Http.Json;
+﻿// using System.Net;
+// using System.Net.Http.Headers;
+// using System.Net.Http.Json;
+//
+// namespace IntegrationTests;
+//
+// public class UserEndpointTests : IntegrationTestFixture
+// {
+//     private string _accessToken = null!;
+//
+//     [SetUp]
+//     public new async Task Setup()
+//     {
+//         await base.Setup();
+//
+//         HttpResponseMessage httpResponseMessage = await Client!.PostAsync("api/Login", JsonContent.Create(new
+//         {
+//             Email = "admin@gmail.com",
+//             Password = "Password123!",
+//         }));
+//         _accessToken = (await httpResponseMessage.Content.ReadFromJsonAsync<LoginResponse>())!.AccessToken;
+//
+//         Client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _accessToken);
+//     }
+//
+//     [Test]
+//     public async Task GetAuctions_ShouldReturn200()
+//     {
+//         HttpResponseMessage httpResponseMessage = await Client!.GetAsync("api/v1/User/Auctions/Won");
+//
+//         Assert.That(httpResponseMessage.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+//     }
+// }
 
-namespace IntegrationTests;
-
-public class UserEndpointTests : IntegrationTestFixture
-{
-    private string _accessToken = null!;
-
-    [SetUp]
-    public new async Task Setup()
-    {
-        await base.Setup();
-
-        HttpResponseMessage httpResponseMessage = await Client!.PostAsync("api/Login", JsonContent.Create(new
-        {
-            Email = "admin@gmail.com",
-            Password = "Password123!",
-        }));
-        _accessToken = (await httpResponseMessage.Content.ReadFromJsonAsync<LoginResponse>())!.AccessToken;
-
-        Client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _accessToken);
-    }
-
-    [Test]
-    public async Task GetAuctions_ShouldReturn200()
-    {
-        HttpResponseMessage httpResponseMessage = await Client!.GetAsync("api/v1/User/Auctions/Won");
-
-        Assert.That(httpResponseMessage.StatusCode, Is.EqualTo(HttpStatusCode.OK));
-    }
-}
