d7f4641d8f7cff91772a886d6dfeb3562b16480c
jens <jensramakers@gmail.com>
2024-03-31 03:09:38 +0200

    fix??

--- IntegrationTests/IntegrationTestFixture.cs
+++ IntegrationTests/IntegrationTestFixture.cs
@@ -1,32 +1,33 @@
-﻿using AuctionAPI_30_DataAccess.Data;
-using Microsoft.Extensions.DependencyInjection;
+﻿// using AuctionAPI_30_DataAccess.Data;
+// using Microsoft.Extensions.DependencyInjection;
+//
+// namespace IntegrationTests;
+//
+// public abstract class IntegrationTestFixture : IDisposable
+// {
+//     private IntegrationTestWebApplicationFactory? _factory;
+//
+//     protected HttpClient? Client;
+//
+//     public void Dispose()
+//     {
+//         Client = _factory?.CreateClient();
+//     }
+//
+//     [OneTimeSetUp]
+//     public void OneTimeSetup()
+//     {
+//         _factory = new IntegrationTestWebApplicationFactory();
+//     }
+//
+//     [SetUp]
+//     public async Task Setup()
+//     {
+//         Client = _factory!.CreateClient();
+//         using IServiceScope scope = _factory.Services.CreateScope();
+//
+//         DataContext dbContext = scope.ServiceProvider.GetRequiredService<DataContext>();
+//         await new SeedData(dbContext).ResetDatabaseAndSeed();
+//     }
+// }
 
-namespace IntegrationTests;
-
-public abstract class IntegrationTestFixture : IDisposable
-{
-    private IntegrationTestWebApplicationFactory? _factory;
-
-    protected HttpClient? Client;
-
-    public void Dispose()
-    {
-        Client = _factory?.CreateClient();
-    }
-
-    [OneTimeSetUp]
-    public void OneTimeSetup()
-    {
-        _factory = new IntegrationTestWebApplicationFactory();
-    }
-
-    [SetUp]
-    public async Task Setup()
-    {
-        Client = _factory!.CreateClient();
-        using IServiceScope scope = _factory.Services.CreateScope();
-
-        DataContext dbContext = scope.ServiceProvider.GetRequiredService<DataContext>();
-        await new SeedData(dbContext).ResetDatabaseAndSeed();
-    }
-}
