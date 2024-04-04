232a9e77a483811ae6a5e58b1e20c2604f9796bc
jens <jensramakers@gmail.com>
2024-04-04 23:59:58 +0200

    added validator

--- AuctionAPI_10_Api/Validators/CustomLanguageManager.cs
+++ AuctionAPI_10_Api/Validators/CustomLanguageManager.cs
@@ -11,5 +11,6 @@ public class CustomLanguageManager : LanguageManager
         AddTranslation("en", "NotEmptyValidator", JsonSerializer.Serialize(new { key = "validation.required", propertyName = "{PropertyName}" }));
         AddTranslation("en", "MaximumLengthValidator", JsonSerializer.Serialize(new { key = "validation.max_length", propertyName = "{PropertyName}", maxLength = "{MaxLength}", totalLength = "{TotalLength}" }));
         AddTranslation("en", "GreaterThanValidator", JsonSerializer.Serialize(new { key = "validation.number_min", propertyName = "{PropertyName}", minValue = "{ComparisonValue}" }));
+        AddTranslation("en", "LessThanValidator", JsonSerializer.Serialize(new { key = "validation.number_max", propertyName = "{PropertyName}", maxValue = "{ComparisonValue}" }));
     }
 }
